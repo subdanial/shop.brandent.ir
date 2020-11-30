c7daffc02009e707b621c61507023b36bea6bacb
subdanial <60549151+subdanial@users.noreply.github.com>
2020-11-30 13:12:06 +0330

    Update home.php

--- home.php
+++ home.php
@@ -29,7 +29,7 @@
                 <a href="https://brandent.ir/">
                     <div class="item">
 
-<svg  class="item" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 512.002 512.002" xml:space="preserve">
 <g>
 	<g>
